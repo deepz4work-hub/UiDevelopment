e1c1ba7f9e5e70b214169c60601fe7a9c5ae0f59
Deepz <deepz4work@gmail.com>
2025-09-18 13:33:03 +0530

    cleaning up

--- assignment5-copy/src/pages/Cart.jsx
+++
@@ -1,8 +0,0 @@
-export default function Cart(){
-  return (
-    <div>
-      <h2 className="text-2xl font-bold mb-4">Your Cart</h2>
-      <p className="text-gray-600">Cart is empty — add some products!</p>
-    </div>
-  )
-}
